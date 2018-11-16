926a831b14b23b9e6f9dc0a8fe6281fb2f8262cf
D'Armani Nicholson <44146421+dskvll@users.noreply.github.com>
2018-11-16 16:43:57 -0700

    Sets up requisite framework to begin a mips project.

---
+++ project2.s
@@ -0,0 +1,8 @@
+#mips Project 2 base converter
+.data # Data declaration section
+
+.text # Assembly language instructions
+main: # Start of code section
+
+syscall # call operating system to perform operation
+
