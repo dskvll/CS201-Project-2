80a5bbe7f2280664736a225d184add128679b8c4
D'Armani Nicholson <44146421+dskvll@users.noreply.github.com>
2018-11-16 16:53:50 -0700

    Begins setup
    Stores the strings for the error messages and reserves space for the program to run.

--- project2.s
+++ project2.s
@@ -1,6 +1,9 @@
 #mips Project 2 base converter
 .data # Data declaration section
-
+too_long_input: .asciiz "Input is too long."
+out_of_range: .asciiz "Invalid base-27 number." 
+empty_input: .asciiz "Input is empty."
+user_input: .space 50000
 .text # Assembly language instructions
 main: # Start of code section
 
