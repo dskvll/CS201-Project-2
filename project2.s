31ada4522da3e6414cc63b86d8eef8a8fe00e4b6
D'Armani Nicholson <44146421+dskvll@users.noreply.github.com>
2018-11-16 18:34:10 -0700

    Begins to test for empty string
    this code attempts to test the user input (stored in t7) to see if there are blank spaces

--- project2.s
+++ project2.s
@@ -14,7 +14,7 @@ la $a0, user_input #stores user string into register
 li $a1, 50000 
 syscall # calls previous instructions
 
-add $t3, $0, 0 #initialises register
+add $t7, $0, 0 #initialises register
 add $t7, $0, 0 #initialises register
 la $t0, user_input # copy address of user input into $t0			
 lb $t7,0($t0) # loads the byte value of $t0 into $t7	
@@ -24,10 +24,16 @@ lb $t7,0($t0) # loads the byte value of $t0 into $t7
 beq $t7, 10, No_input_error # branches if $t7 is a new line command	
 beq $t7, 0 No_input_error # branches if there is literally no input in $t7	
 
-addi $s1, $0, 27 #initialises the register with desired base
-addi $t5, $0, 0 	#initialises register for future use
-addi $t4, $0, 0	 # initialises register for use
+addi $s0, $0, 27 #initialises the register with desired base
+addi $t6, $0, 0 	#initialises register for future use
+addi $t5, $0, 0	 # initialises register for use
 addi $t1, $0, 1 	#initializes register for future use
+
+space_ignore:
+	lb $t7,0($t0)# loads the byte value of $t0 into $t7	
+	addi $t0, $t0, 1# initialises count to 1
+	addi $t3, $t3, 1# initialises count to 1
+	beq $t7, 32, space_ignore # if the user input is a space then we run the ignore program
 
 li $v0,10 #ends program
 syscall # call operating system to perform operation
